ec9cce23205ad8c7f4f0806a7deb956ebf9720c3
ASUS <ASUS@Tetyana>
2020-11-16 19:59:29 +0200

    added first project to github

---
+++ lesson  1/src/Tandem20/Application.java
@@ -0,0 +1,27 @@
+package Tandem20;
+
+public class Application {
+	public static void main(String[] args) {
+		int array1[] = new int[10];
+		double[] array2 = new double[10];
+
+		for (int i = 0; i < array1.length; i++) {
+			array1[i] = i;
+			System.out.println(array1[i] + "");
+		}
+		Byte a1 = 2;
+		Short a2 = 2;
+		Integer a3 = 2;
+		Long a4 = 2L;
+
+		System.out.println("Byte = " + Byte.MAX_VALUE);
+		System.out.println("Byte = " + Byte.MIN_VALUE);
+		System.out.println("Short = " + Short.MAX_VALUE);
+		System.out.println("Short = " + Short.MIN_VALUE);
+		System.out.println("Integer = " + Integer.MAX_VALUE);
+		System.out.println("Integer = " + Integer.MIN_VALUE);
+		System.out.println("Long = " + Long.MAX_VALUE);
+		System.out.println("Long = " + Long.MIN_VALUE);
+
+	}
+}
